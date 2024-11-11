7756dfabccdd31015749fa1c742c1fbcb398f271
Zakilay <ilocodes21@gmail.com>
2024-11-11 16:27:57 +0400

    Create number.swift

---
+++ Day 1/number.swift
@@ -0,0 +1,27 @@
+let score = 10
+
+let reallyBig = 100000000
+let reallyBig = 100_000_000
+let reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+counter += 5
+print(counter)
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
