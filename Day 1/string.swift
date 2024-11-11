6266f6ea286e48141fa2124cc55fb64f5f69f254
Zakilay <ilocodes21@gmail.com>
2024-11-11 16:05:22 +0400

    Create string.swift

---
+++ Day 1/string.swift
@@ -0,0 +1,22 @@
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let movie = "A day in
+the life of an
+Apple engineer"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
