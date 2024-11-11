e738a6a7e182707a49ab62bfbb2dce8cceaf1168
Zakilay <ilocodes21@gmail.com>
2024-11-11 16:00:23 +0400

    Create variable.swift

---
+++ variable.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+let character = "Daphne"
+character = "Eloise"
+character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
