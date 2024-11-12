5f0f40323426116c67a892b903fc4d9fccb4d157
Zakilay <ilocodes21@gmail.com>
2024-11-12 14:36:56 +0400

    Create checkpoint1.swift

---
+++ Day 2/checkpoint1.swift
@@ -0,0 +1,8 @@
+// Temperature Converter: Celsius to Fahrenheit
+
+import UIKit
+
+let celsius = 25.0
+let fahrenheit = celsius * 9 / 5 + 32
+
+print(" Temperate in Celsius \(celsius)° & Fahrenheit \(fahrenheit)°F")
